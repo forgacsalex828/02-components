3fcab59974ebd6580460eca837faaea67a77e885
Forgacs Alex <83586960+forgacsalex828@users.noreply.github.com>
2026-09-23 13:04:10 +0200

    init

---
+++ 01-create-components/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import MainNavigation from './components/MainNavigation.vue';
+</script>
+
+<template>
+    <header>
+        <MainNavigation />
+    </header>
+</template>
+
+<style scoped>
+</style>
+
+
+<style scoped>
+    nav {
+        background-color: #333;
+        color: #fff;
+        padding: 1rem;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        gap: 1rem;
+    }
+
+    li {
+        margin: 0;
+    }
+
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+    
+</style>
